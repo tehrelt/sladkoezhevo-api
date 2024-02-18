6c79aa191b11b6c303b29024d8bb43c47baa6428
relt <haxrelt@gmail.com>
2024-02-18 20:44:08 +0300

    added logger setup

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sladkoezhevo-api/internal/config"
+)
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	config := config.NewConfig("config/.yaml")
+	logger := setupLogger(config.Env)
+	logger.Debug("Logger initialized")
+}
 
-	fmt.Print(config)
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
